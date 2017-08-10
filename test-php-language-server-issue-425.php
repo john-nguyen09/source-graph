01b823535f0cea04685f172494d54a08636d4511
John Nguyen <john.nguyen09@outlook.com>
2017-08-10 13:06:49 +1000

    Add php language server test with sourcegraph

---
+++ test-php-language-server-issue-425.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Get courses report for user
+ *
+ * @param stdClass|int|null $user User id or user object with id or null if get from current user
+ * @return array
+ */
+function get_user_latest_report($user = null) {
+    $user->
+}
